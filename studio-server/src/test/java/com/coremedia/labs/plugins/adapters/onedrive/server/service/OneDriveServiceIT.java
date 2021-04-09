b51342d6b8cc26feec4266113849d0d5050df529
sbuettne <sebastian.buettner@coremedia.com>
2021-04-09 17:27:00 +0200

    Fixing http errors by adding missing runtime dependency to jersey-client jersey-client is required by the microsoft-graph library REST requests and must be provided during runtime

--- studio-server/src/test/java/com/coremedia/labs/plugins/adapters/onedrive/server/service/OneDriveServiceIT.java
+++ studio-server/src/test/java/com/coremedia/labs/plugins/adapters/onedrive/server/service/OneDriveServiceIT.java
@@ -6,6 +6,7 @@ import com.microsoft.graph.models.extensions.Site;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.InputStream;
 import java.util.List;
 
 import static org.junit.Assert.assertNotNull;
@@ -20,6 +21,9 @@ public class OneDriveServiceIT {
   private static final String SHARE_POINT_SITE = "cmpresales.sharepoint.com:/sites/Sample";
   private static final String SHARE_POINT_SITE_ID = "cmpresales.sharepoint.com,21c06144-5038-4ba3-8b99-115b661f9d09,4393c333-c93a-45e5-aaf7-39cf66c1de1d";
   private static final String DRIVE_ID = "b!RGHAIThQo0uLmRFbZh-dCTPDk0M6yeVFqvc5z2bB3h15UA6_MMe4T46v9u3_EE7S";
+
+  // Example asset:
+  // https://cmpresales.sharepoint.com/sites/Sample/Shared%20Documents/Assets/Summer%20Accessories%202.jpeg
   private static final String DRIVE_ITEM_ID = "01CL5ITZYQXUNCRR266VFJ7GYKWP22GNWF";
 
   @Before
@@ -62,6 +66,13 @@ public class OneDriveServiceIT {
   public void testGetItem() throws Exception {
     DriveItem item = testling.getItem(DRIVE_ID, DRIVE_ITEM_ID);
     assertNotNull(item);
+  }
+
+  @Test
+  public void testGetDownloadStream() throws Exception {
+    DriveItem item = testling.getItem(DRIVE_ID, DRIVE_ITEM_ID);
+    InputStream downloadStream = testling.getDownloadStream(item);
+    assertNotNull(downloadStream);
   }
 
 }
